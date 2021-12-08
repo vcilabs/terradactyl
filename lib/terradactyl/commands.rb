7a14411f9a96d0e4acd60905fb06aab3e6bbb74e
Wade Peacock <wade.peacock@alida.com>
2021-12-08 15:58:42 -0800

    Terradactyl - Add terraform 1.1.0 support
    New 1.1.x release available
    
    Update depends in terradactyl-terraform to >= 1.1.0
    
    Set rev1_00 as upgradeable

--- lib/terradactyl/commands.rb
+++ lib/terradactyl/commands.rb
@@ -403,6 +403,22 @@ module Terradactyl
         Terraform::Rev1_00::PlanFileParser
       end
     end
+
+    module Rev1_01
+      class << self
+        def upgradeable?
+          false
+        end
+      end
+
+      include Terraform::Commands
+
+      private
+
+      def parser
+        Terraform::Rev1_01::PlanFileParser
+      end
+    end
   end
   # rubocop:enable Metrics/ModuleLength
 end
